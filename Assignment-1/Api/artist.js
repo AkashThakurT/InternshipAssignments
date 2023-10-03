8370d6277b2a5f8eee7ac02c298ca132eca24a1a
AkashThakurT <akash.thakur@prodevans.com>
2023-10-04 00:34:44 +0530

    updated

--- Assignment-1/Api/artist.js
+++ Assignment-1/Api/artist.js
@@ -1,4 +1,4 @@
-// fetch the data from spotify
+// fetch the data from spotify data
 const token = 'BQDUEAavQYOE3vVHDZP0-ryc5M_Bi3J3N-Po6L30yRh2jHROYXZhE8Oz_4tFZPnEUBzWZBEtHSiGvoOfncVyjZpFMLcayZtwxINlGxJ0266hE8WOP9d3DQq0yTVVSjXY9nKos48Th5i7GRlN57cyeXumpR5T0JcN4undF1hfe20Gq8dLR-KoVIWeCy1KATiD_rp3xVjbMtozD5xlkKbNVWigBxoMeIF6tDEA_9g7615nlQbFAL59TlG1DFHL4IPuyIsPxFWjFxnS29LRZRicpYUo';
 const url = "https://api.spotify.com/v1/artists?ids=4YRxDV8wJFPHPTeXepOstw%2C1SJOL9HJ08YOn92lFcYf8a%2C1dVygo6tRFXC8CSWURQJq2%2C0GF4shudTAFv8ak9eWdd4Y%2C4K6blSRoklNdpw4mzLxwfn%2C0oOet2f43PA68X5RxKobEy%2C4fEkbug6kZzzJ8eYX6Kbbp%2C4K6blSRoklNdpw4mzLxwfn%2C5f4QpKfy7ptCHwTqspnSJI%2C0GF4shudTAFv8ak9eWdd4Y"
 const request = new Request(
